5c7be4453ed4f75f32a093ccab46ebf50377610f
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-09-25 10:44:38 -0500

    removed questions from app.js file

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
 const fs = require("fs");
+
 const inquirer = require("inquirer");
-const inquire = require("inquirer");
-
-
-
-inquirer.prompt([{
-        type: "input",
-        name: "manager",
-        message: "Manager name?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Managers email?"
-    }
-]);
