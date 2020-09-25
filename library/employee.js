371c3602679206492cb5c575e434fb96efa56d5b
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-09-25 11:52:20 -0500

    added class and constructor to employee

---
+++ library/employee.js
@@ -0,0 +1,26 @@
+`use strict`;
+
+class employee {
+    constructor(name, ID, email) {
+        this.name = name;
+        this.ID = ID;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.ID;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Employee";
+    }
+}
+module.exports = employee;
